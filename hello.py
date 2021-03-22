bddef26a16ad11db24c81180ee3ab86b521348a4
Akshat Mohan Awasthi <akshatmawasthi@gmail.com>
2021-03-22 14:19:04 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,5 +0,0 @@
-// Execute Python Scripts
-// Not using any python editor, using vim to type out this code.
-// Using python on terminal to check for output, installed Python 3.8
-
-print("hello, world!")
